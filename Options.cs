8c869c59799a78910036038272ece1f6eb0ab77f
Alfonso Siloniz <alfonso.siloniz.ext@telefonica.com>
2024-02-29 19:46:25 +0100

    FPL2SCN now it is using the library BGLLibrary. Many changes in all parts of the code

--- Options.cs
+++ Options.cs
@@ -4,11 +4,13 @@ namespace VisualPointsNamespace {
 
     public class Options
     {
-        [Option('i', "input", Required = true, HelpText = "Input file path + name.")]
-        public string InputFilePath { get; set; }
+        [Value(0, Required = true, HelpText = "Input file path + name")]
+        public string InputFilePath {get; set;}
 
         [Option('o', "output", Required = false, HelpText = "Output file path.")]
         public string OutputFilePath { get; set; }
 
+        [Option('x', "xml", Required = false, HelpText = "Output file path.")]
+        public bool CreateXML { get; set; } = false;
     }
 }
