72a64442b1b323f82c999bbc9da71bee7d0cb87e
Alfonso Siloniz <alfonso.siloniz.ext@telefonica.com>
2024-03-04 16:56:49 +0100

    Fixes in FPL2SCN for new configuration file. Added computation of header QMIDs in BGL when more than 8 QMIDs are present in Library Objects

--- BGLLibrary/Bgl.cs
+++ BGLLibrary/Bgl.cs
@@ -67,9 +67,7 @@ public class SceneryObjectSection {
 
     UInt32 Type = 0x25;
     UInt32 SubSectionSize = 0x01;
-    UInt32 NumbeOfSubsections = 0;
     public UInt32 SubsectionFileOffset = 0;
-    UInt32 TotalSizeSubsections = 0;
 
     public void AddSubSection(SubSection SubSec) {
         this.SubSections.Add(SubSec);
@@ -259,6 +257,8 @@ public class Bgl
     Logger logger; 
     string filename;
 
+    List<QMID> SubSectionsQMIDs = new();
+
     public Bgl(string filename) {
         // Create a First Section in this case
         this.Sections.Add(new SceneryObjectSection());
@@ -286,6 +286,8 @@ public class Bgl
         
         // We compute the QMID to group this object in Subsection
         (uint u, uint v, uint l) = Util.CalcQmidFromCoord(Object.lat, Object.lon, 11);
+        this.SubSectionsQMIDs.Add(new Bgl.QMID(u,v,l));
+
         UInt32 QMID = Util.GetQMIDDWORDs(u,v,(int)l);
         // Look for a subsection with this QMID
         IEnumerable<SubSection> result = [];
@@ -348,6 +350,7 @@ public class Bgl
                 QMID_0 = Util.GetQMIDDWORDs(u,v,(int)l);
 
                 // TEST OTHER ALGORITHM < THIS IS THE GOOD ONE
+                // TODO Clean this code
                 uint deltaLevel = ltmp - 9;
                 uint uprima = utmp >> (int)deltaLevel;
                 uint vprima = vtmp >> (int)deltaLevel;
@@ -359,18 +362,147 @@ public class Bgl
                 }
             }    
 
-            int i=0;
-            foreach (UInt32 QMID in RecomputedQMIDs) {
-                this.Header.QMIDs[i] = QMID;
-                i++;
+            // First part of Header QMID Algorithm. Number QMIDs < 9
+            if (RecomputedQMIDs.Count < 9) 
+            {
+                int i=0;
+                foreach (UInt32 QMID in RecomputedQMIDs) {
+                    if (i<8) {
+                        this.Header.QMIDs[i] = QMID;
+                    }
+                    i++;
+                }
+            } 
+            else 
+            {
+                // More than 8 header QMIDs. We need to find the minimum that
+                // cover all the scenery area
+
+                List<QMID> headerQMIDs = new List<QMID>();
+                headerQMIDs.Add( new QMID(0,0,0));
+
+                bool entryAdded = false;
+                int iteration = 0;
+
+                do {
+                    List<QMID> tmpQMIDs = new List<QMID>(headerQMIDs);
+
+                    entryAdded = false;
+                    
+                    logger.Debug("Iteration: {0} // Count: {1}, {2}",  iteration++, headerQMIDs.Count(), tmpQMIDs.Count());
+                    foreach (QMID qmid in headerQMIDs) {
+                        logger.Debug("{0}", qmid);
+                    }
+                    Console.WriteLine("**********************");
+                    // foreach (QMID qmid in tmpQMIDs) 
+                    int tmpIndex = 0;
+                    foreach ((QMID qmid, int index) in tmpQMIDs.Select((item, index) => (item, index)))
+                    {
+                        logger.Debug("Index{0} QMID: {1}", index, qmid);
+                        if (qmid.L < 9) 
+                        {
+                            // compute subQmids
+                            // uprima = { u*2 , u*2 + 1 }
+                            // vprima = { v*2 , v*2 + 1}
+                            var match = new List<QMID>();
+                            for (int i=0; i<2; i++) {
+                                for (int j=0; j<2; j++) {
+
+                                    QMID evalQMID = new((uint)(qmid.U * 2 + i), (uint)(qmid.V * 2 + j), qmid.L + 1);
+
+                                    bool localMatchQMID (QMID subqmid, List<QMID> existingQmids ) {
+                                        // Complex evaluation of QMIDs
+                                        foreach (QMID qmid in existingQmids) 
+                                        {
+                                            int deltaLevel = (int)qmid.L - (int)subqmid.L;
+                                            if (deltaLevel < 0) 
+                                            {
+                                                deltaLevel = -deltaLevel;
+                                                uint U1 = subqmid.U >> deltaLevel;
+                                                uint V1 = subqmid.V >> deltaLevel;
+
+                                                if (U1 == qmid.U && V1 == qmid.V) 
+                                                    return true;
+                                            } 
+                                            else 
+                                            {
+                                                uint U1 = qmid.U >> deltaLevel;
+                                                uint V1 = qmid.V >> deltaLevel;
+
+                                                if (U1 == subqmid.U && V1 == subqmid.V) 
+                                                    return true;
+
+                                            }
+
+
+                                        }
+
+                                        return false;
+                                    }
+                                               
+                                    // subQmid Match
+                                    var OrderedSubSectionsQMIDs = SubSectionsQMIDs.OrderBy( u => u.QMIDWORD).ToList();
+
+                                    if (localMatchQMID(evalQMID, SubSectionsQMIDs)) {
+                                        match.Add(evalQMID);
+                                        logger.Debug("subqmid MATCHED {0}", evalQMID);
+                                    } else {
+                                        logger.Debug("subqmid NOT MATCHED {0}", evalQMID);
+                                    }
+                                }
+                            }
+                                if (match.Count == 1) 
+                                {
+                                    // Only one match. We replace this element in the header List
+                                    logger.Debug("REPLACED QMID {0} with QMID {1} in index {2}", qmid, match[0], tmpIndex);
+                                    headerQMIDs[tmpIndex] = match[0];
+                                    entryAdded = true;
+                                }
+                                else
+                                {
+                                    if (match.Count == 0) {
+                                        logger.Debug("QMID MOT FOUND {0}", qmid );
+                                        break;
+                                    }
+                                    else 
+                                    {
+                                        if (headerQMIDs.Count() + match.Count() < 10  ) {
+                                            logger.Debug("Removing original element at index {0}", tmpIndex);
+                                            headerQMIDs.RemoveAt(tmpIndex);
+                                            logger.Debug("Inserting the following list in index {0}", tmpIndex);
+                                            int z = tmpIndex;
+                                            tmpIndex--;
+                                            foreach (QMID m in match) {
+                                                logger.Debug("{0} at index {1}", m, z);
+                                                headerQMIDs.Insert(z++,m);
+                                                tmpIndex++;
+                                            }
+                                            entryAdded = true;
+                                        }
+                                        else 
+                                        {
+                                            logger.Debug("More than 8 QMIDs: Found  {0} ",  match.Count() );
+                                        }
+                                    }
+                                }                            
+                        }
+                        tmpIndex++;
+                    }
+
+                } while (entryAdded && iteration <50);
+
+                logger.Debug("FINAL QMID HEADER LIST");
+                int w = 0;
+                foreach (QMID qmid in headerQMIDs) 
+                {
+                    this.Header.QMIDs[w++] = Util.GetQMIDDWORDs(qmid.U, qmid.V, (int)qmid.L);
+                    logger.Debug("QMID: {0}",qmid);
+                }
+
+
             }
 
         }
-
-        // TODO: Compute Header QMIDS algorithm
-        // TODO: We need to add what happens if there more than 8 QMIDs in the header
-
-
 
         // Now we have all FileOffsets defined. We can build up the BGL File
         var bw = new BinaryWriter(File.Open(this.filename,FileMode.Truncate));
@@ -415,5 +547,24 @@ public class Bgl
             }
         }
         File.WriteAllText(jsonFile, layout.ToString());
+    }
+
+    private struct QMID {
+        public uint U;
+        public uint V;
+        public uint L;
+        public QMID (uint u, uint v, uint l) {
+            U = u;
+            V = v;
+            L = l;
+
+            QMIDWORD = Util.GetQMIDDWORDs(U,V,(int)L);
+        }
+        public UInt32 QMIDWORD = 0;
+
+        override public string  ToString() {
+            return "("+U.ToString()+","+V.ToString()+","+L.ToString()+")";
+
+        }
     }
 }
